e54328703f8abb528a28c1f5632330a7c64f6c5b
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-02-25 14:50:26 +0530

    Initial Commit

---
+++ TableViewMultipleSelectionUsingTwoFingers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewMultipleSelectionUsingTwoFingers
+//
+//  Created by shiga on 25/02/20.
+//  Copyright © 2020 shiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
